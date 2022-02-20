9f2289a305141890a430b7d5f6832166ef60a6b9
serghei.morari <serghei.morari@amdaris.com>
2022-02-21 00:19:48 +0200

    Initial commit

---
+++ src/main/java/com/example/springgraphqlsecurity/SpringAuthorizationWithGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgraphqlsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationWithGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthorizationWithGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springgraphqlsecurity/SpringAuthorizationWithGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgraphqlsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationWithGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
